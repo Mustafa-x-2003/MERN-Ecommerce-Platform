bebc3ff2463dff41de683f5bec570933a8a5358e
Nagham <mostafaa01094803310@gmail.com>
2026-07-30 01:27:49 +0300

    feat(auth): add JWT authentication middleware

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { register, login } from "../controllers/userControllers.js";
+import { register, login ,getProfile} from "../controllers/userControllers.js";
+import auth from "../middleware/auth.js";
 const routerUser = express.Router();
 
 routerUser.post("/register", register);
 routerUser.post("/login", login);
+routerUser.get("/profile",auth, getProfile);
 
 export default routerUser;
