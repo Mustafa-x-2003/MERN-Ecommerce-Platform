ecdd9859371cca864dd6f91ed8a3816d19aa8ab1
Nagham <mostafaa01094803310@gmail.com>
2026-07-29 21:13:43 +0300

    chore: initialize project structure

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import express from "express";
+import concetDB from "./config/db.js";
+import cors from "cors";
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, async () => {
+  try {
+    await concetDB();
+    console.log(`server is running on port ${port}`);
+  } catch (e) {
+    console.log(e);
+  }
+});
