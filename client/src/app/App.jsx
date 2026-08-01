509ee71ce77b44c63e9469cefd13c2963299c390
Nagham <mostafaa01094803310@gmail.com>
2026-08-01 22:34:32 +0300

    chore: setup frontend project architecture

---
+++ client/src/app/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Providers from "./Providers";
+import AppRoutes from "../routes/AppRoutes";
+// =====================================
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export default function App() {
+  return (
+    <Providers>
+      <AppRoutes />
+      <ToastContainer position="top-right" autoClose={2000} />
+    </Providers>
+  );
+}
