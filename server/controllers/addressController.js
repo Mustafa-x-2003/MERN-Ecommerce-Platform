231ea9815cdc81b27a0405c1b744fd3f66670239
Nagham <mostafaa01094803310@gmail.com>
2026-08-03 16:27:57 +0300

    feat: add update address controller

--- server/controllers/addressController.js
+++ server/controllers/addressController.js
@@ -20,12 +20,6 @@ export const getAddresses = async (req, res) => {
   try {
     const { _id } = req.user;
     const addresses = await Address.find({ user: _id });
-    if (!addresses.length) {
-      return res.status(404).json({
-        message: "No addresses found",
-      });
-    }
-
     return res.status(200).json({
       message: "Addresses retrieved successfully",
       addresses,
@@ -53,6 +47,62 @@ export const deleteAddres = async (req, res) => {
   } catch (error) {
     res.status(500).json({
       message: "Failed to delete address",
+      error: error.message,
+    });
+  }
+};
+export const editAddress = async (req, res) => {
+  try {
+    const updates = Object.keys(req.body);
+    if (updates.length === 0) {
+      return res.status(400).json({
+        message: "No updates provided",
+      });
+    }
+
+    const allowedUpdates = [
+      "name",
+      "phone",
+      "country",
+      "city",
+      "street",
+      "building",
+      "apartment",
+      "postalCode",
+      "isDefault",
+    ];
+    const areUpdatesAllowed = updates.every((item) =>
+      allowedUpdates.includes(item),
+    );
+    if (!areUpdatesAllowed) {
+      return res.status(400).json({
+        message: "Invalid update field",
+      });
+    }
+
+    const address = await Address.findOne({
+      _id: req.params.id,
+      user: req.user._id,
+    });
+    if (!address) {
+      return res.status(404).json({
+        message: "Address not found",
+      });
+    }
+
+    updates.forEach((item) => {
+      address[item] = req.body[item];
+    });
+
+    await address.save();
+
+    res.status(200).json({
+      message: "Address updated successfully",
+      address,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
       error: error.message,
     });
   }
