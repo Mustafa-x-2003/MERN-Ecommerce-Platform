a4e1963b505c6f00e98cf5f35398782b81629c24
Nagham <mostafaa01094803310@gmail.com>
2026-08-07 23:34:50 +0300

    feat: implement update cart quantity

--- server/router/cartRouter.js
+++ server/router/cartRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import auth from "../middleware/auth.js";
-import { addToCart, getCart } from "../controllers/cartController.js";
+import { addToCart, getCart ,updateCartQuantity} from "../controllers/cartController.js";
 
 const cartRouter = express.Router();
 cartRouter.post("/:id",auth, addToCart);
 cartRouter.get("/",auth, getCart);
+cartRouter.patch("/quantity",auth, updateCartQuantity);
 export default cartRouter
